c81f887d044405ae7e3486c9dbd2ba6c91f70f17
Nick Levashov <niklevashov@yandex.ru>
2015-10-27 15:40:43 +0300

    Interfaces

--- interfaces.hpp
+++ interfaces.hpp
@@ -62,7 +62,41 @@ class VarDeclaration : public IVarDeclaration {
 
 //-----------------------------------------------------
 
-class MethodDeclaration : public IMethodDeclaration {
+class VarDeclarationStar1 : public IVarDeclarationStar {
+
+};
+
+class VarDeclarationStar2 : public IVarDeclarationStar {
+
+};
+
+//-----------------------------------------------------
+
+class MethodDeclaration1 : public IMethodDeclaration {
+
+};
+
+class MethodDeclaration2 : public IMethodDeclaration {
+
+};
+
+//-----------------------------------------------------
+
+class MethodDeclarationStar1 : public IMethodDeclarationStar {
+
+};
+
+class MethodDeclarationStar2 : public IMethodDeclarationStar {
+
+};
+
+//-----------------------------------------------------
+
+class CommaTypeIdentifierStar1 : public ICommaTypeIdentifierStar {
+
+};
+
+class CommaTypeIdentifierStar2 : public ICommaTypeIdentifierStar {
 
 };
 
@@ -86,10 +120,6 @@ class TypeIdentifier : public IType {
 
 //-----------------------------------------------------
 
-class StatementStar : public IStatement {
-
-};
-
 class StatementIf : public IStatement {
 
 };
@@ -103,5 +133,99 @@ class StatementPrint : public IStatement {
 };
 
 class StatementIdentifier : public IStatement {
+
+};
+
+//-----------------------------------------------------
+
+class StatementStar1 : public IStatementStar {
+
+};
+
+class StatementStar2 : public IStatementStar {
+
+};
+
+//-----------------------------------------------------
+
+class ExpressionAndAnd : public IExpression {
+
+};
+
+class ExpressionLessThen : public IExpression {
+
+};
+
+class ExpressionPlus : public IExpression {
+
+};
+
+class ExpressionMinus : public IExpression {
+
+};
+
+class ExpressionMult : public IExpression {
+
+};
+
+class ExpressionBracks : public IExpression {
+
+};
+
+class ExpressionLength : public IExpression {
+
+};
+
+class ExpressionMethod : public IExpression {
+
+};
+
+class ExpressionEmptyMethod : public IExpression {
+
+};
+
+class ExpressionInt : public IExpression {
+
+};
+
+class ExpressionTrue : public IExpression {
+
+};
+
+class ExpressionFalse : public IExpression {
+
+};
+
+class ExpressionId : public IExpression {
+
+};
+
+class ExpressionThis : public IExpression {
+
+};
+
+class ExpressionNew : public IExpression {
+
+};
+
+class ExpressionEmptyNew : public IExpression {
+
+};
+
+class ExpressionNot : public IExpression {
+
+};
+
+class ExpressionParens : public IExpression {
+
+};
+
+//-----------------------------------------------------
+
+class CommaExpressionStar1 : public ICommaExpressionStar {
+
+};
+
+class CommaExpressionStar2 : public ICommaExpressionStar {
 
 };
