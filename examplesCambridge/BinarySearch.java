9dff544f76983ba962efd6535772e5aa71b72184
mashaka <mashasandrikova@gmail.com>
2015-12-14 18:39:48 +0300

    Delete comment from example

--- examplesCambridge/BinarySearch.java
+++ examplesCambridge/BinarySearch.java
@@ -3,16 +3,11 @@ class BinarySearch{
 	System.out.println(new BS().Start(20));
     }
 }
-// This class contains an array of integers and
-// methods to initialize, print and search the array
-// using Binary Search
 
 class BS{
     int[] number ;
     int size ;
 
-    // Invoke methods to initialize, print and search
-    // for elements on the array
     public int Start(int sz){
 	int aux01 ;
 	int aux02 ;
@@ -39,8 +34,6 @@ class BS{
     }
 
 
-    // Search for a specific value (num) using
-    // binary search
     public boolean Search(int num){
 	boolean bs01 ;
 	int right ;
@@ -73,8 +66,6 @@ class BS{
 	return bs01 ;
     }
 
-    // This method computes and returns the
-    // integer division of a number (num) by 2
     public int Div(int num){
 	int count01 ;
 	int count02 ;
@@ -91,9 +82,6 @@ class BS{
     }
 
     
-    // This method compares two integers and
-    // returns true if they are equal and false
-    // otherwise
     public boolean Compare(int num1 , int num2){
 	boolean retval ;
 	int aux02 ;
@@ -106,7 +94,6 @@ class BS{
 	return retval ;
     }
 
-    // Print the integer array
     public int Print(){
 	int j ;
 
@@ -120,7 +107,6 @@ class BS{
     }
     
 
-    // Initialize the integer array
     public int Init(int sz){
 	int j ;
 	int k ;
