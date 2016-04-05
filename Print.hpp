0ae6f82c7bfcfe115c5603a1596e52bf89bd55c4
Nick Levashov <niklevashov@yandex.ru>
2016-04-05 15:27:25 +0300

    Fix print

--- Print.hpp
+++ Print.hpp
@@ -1,8 +1,14 @@
 #include "TempMap.hpp"
 #include "DefaultMap.hpp"
-
-#include "Label.hpp"
 #include "Exp.hpp"
+#include "Stm.hpp"
+
+#include "SEQ.hpp"
+#include "LABEL.hpp"
+#include "JUMP.hpp"
+#include "CJUMP.hpp"
+#include "MOVE.hpp"
+#include "EXP.hpp"
 
 #include <memory>
 #include <iostream>
@@ -13,15 +19,28 @@ namespace Tree {
 	class Print {
 		ostream out;
 		Temp::TempMap tmap;
+
 	public:
 		Tree(std::ostream &_out, std::shared_ptr<Temp::TempMap> _tmap):
 		out(_out),
 		tmap(_tmap)
 		{}
 
-		Tree(std::ostream &_out, std::shared_ptr<Temp::TempMap> _tmap):
+		Tree(std::ostream &_out):
 		out(_out),
 		{
+			tmap = Temp::DefaultMap();
+		}
+
+		Tree(std::shared_ptr<Temp::TempMap> _tmap):
+		tmap(_tmap)
+		{
+			out = std::cout;
+		}
+
+		Tree():
+		{
+			out = std::cout;
 			tmap = Temp::DefaultMap();
 		}
 
@@ -40,20 +59,20 @@ namespace Tree {
 		}
 
 
-		void prStm(SEQ s, int d=0) {
-			indent(d); sayln("SEQ("); prStm(s.left, d+1); sayln(",");
-			prStm(s.right, d+1); say(")");
+		void print(SEQ s, int d=0) {
+			indent(d); sayln("SEQ("); print(s.left, d+1); sayln(",");
+			print(s.right, d+1); say(")");
 		}
 
-		void prStm(LABEL s, int d=0) {
+		void print(LABEL s, int d=0) {
 			indent(d); say("LABEL "); say(s.label.toString());
 		}
 
-		void prStm(JUMP s, int d=0) {
-			indent(d); sayln("JUMP("); prExp(s.exp, d+1); say(")");
+		void print(JUMP s, int d=0) {
+			indent(d); sayln("JUMP("); print(s.exp, d+1); say(")");
 		}
 
-		void prStm(CJUMP s, int d=0) {
+		void print(CJUMP s, int d=0) {
 			indent(d); say("CJUMP("); 
 			switch(s.relop) {
 				case CJUMP.EQ:  say("EQ");  break;
@@ -68,79 +87,80 @@ namespace Tree {
 				case CJUMP.UGE: say("UGE"); break;
 				default: throw "Wrong CJUMP field";
 			}
-			sayln(","); prExp(s.left, d+1); sayln(",");
-			prExp(s.right, d+1); sayln(",");
+			sayln(","); print(s.left, d+1); sayln(",");
+			print(s.right, d+1); sayln(",");
 			indent(d+1); say(s.iftrue.toString()); say(",");
 			say(s.iffalse.toString()); say(")");
 		}
 
-		void prStm(MOVE s, int d=0) {
-			indent(d); sayln("MOVE("); prExp(s.dst, d+1); sayln(",");
-			prExp(s.src, d+1); say(")");
+		void print(MOVE s, int d=0) {
+			indent(d); sayln("MOVE("); print(s.dst, d+1); sayln(",");
+			print(s.src, d+1); say(")");
 		}
 
-		void prStm(EXP s, int d=0) {
-			indent(d); sayln("EXP("); prExp(s.exp, d+1); say(")");
+		void print(EXP s, int d=0) {
+			indent(d); sayln("EXP("); print(s.exp, d+1); say(")");
 		}
 
-		// don't need this. hallelujah to overload
-		// void prStm(Stm s, int d=0) {}
-
-		void prExp(BINOP e, int d=0) {
+		void print(BINOP e, int d=0) {
 			indent(d); say("BINOP("); 
 			switch(e.binop) {
-				case BINOP.PLUS: say("PLUS"); break;
-				case BINOP.MINUS: say("MINUS"); break;
-				case BINOP.MUL: say("MUL"); break;
-				case BINOP.DIV: say("DIV"); break;
-				case BINOP.AND: say("AND"); break;
-				case BINOP.OR: say("OR"); break;
-				case BINOP.LSHIFT: say("LSHIFT"); break;
-				case BINOP.RSHIFT: say("RSHIFT"); break;
+				case BINOP.PLUS:    say("PLUS");    break;
+				case BINOP.MINUS:   say("MINUS");   break;
+				case BINOP.MUL:     say("MUL");     break;
+				case BINOP.DIV:     say("DIV");     break;
+				case BINOP.AND:     say("AND");     break;
+				case BINOP.OR:      say("OR");      break;
+				case BINOP.LSHIFT:  say("LSHIFT");  break;
+				case BINOP.RSHIFT:  say("RSHIFT");  break;
 				case BINOP.ARSHIFT: say("ARSHIFT"); break;
-				case BINOP.XOR: say("XOR"); break;
+				case BINOP.XOR:     say("XOR");     break;
 				default: throw "Wrong BINOP field";
 			}
 			sayln(",");
-			prExp(e.left,d+1); sayln(","); prExp(e.right, d+1); say(")");
+			print(e.left, d+1); sayln(","); print(e.right, d+1); say(")");
 		}
 
-		void prExp(MEM e, int d=0) {
+		void print(MEM e, int d=0) {
 			indent(d);
-			sayln("MEM("); prExp(e.exp,d+1); say(")");
+			sayln("MEM("); print(e.exp, d+1); say(")");
 		}
 
-		void prExp(TEMP e, int d=0) {
+		void print(TEMP e, int d=0) {
 			indent(d); say("TEMP "); 
 			say(tmap.tempMap(e.temp));
 		}
 
-		void prExp(ESEQ e, int d=0) {
-			indent(d); sayln("ESEQ("); prStm(e.stm, d+1); sayln(",");
-			prExp(e.exp, d+1); say(")");
+		void print(ESEQ e, int d=0) {
+			indent(d); sayln("ESEQ("); print(e.stm, d+1); sayln(",");
+			print(e.exp, d+1); say(")");
 		}
 
-		void prExp(NAME e, int d=0) {
+		void print(NAME e, int d=0) {
 			indent(d); say("NAME "); say(e.label.toString());
 		}
 
-		void prExp(CONST e, int d=0) {
-			indent(d); say("CONST "); say(String.valueOf(e.value));
+		void print(CONST e, int d=0) {
+			indent(d); say("CONST "); say(std::string(e.value));
 		}
 
-		void prExp(CALL e, int d=0) {
+		void print(CALL e, int d=0) {
 			indent(d); sayln("CALL(");
-			prExp(e.func, d+1);
-			// rewrite
-			for(ExpList a = e.args; a!=null; a=a.tail) {
-				sayln(","); prExp(a.head, d+2); 
+			print(e.func, d+1);
+			for(ExpList a = e.args; a != nullptr; a = a.tail) {
+				sayln(","); print(a.head, d+2); 
 			}
 			say(")");
 		}
 
 		// don't need this. hallelujah to overload
-		// void prExp(Exp e, int d=0) {}
-		// void prStm(Stm s) {prStm(s, 0); say("\n");}
-		// void prExp(Exp e) {prExp(e, 0); say("\n");}
+		// void print(Stm s, int d=0) {}
+		// void print(Exp e, int d=0) {}
+		// void print(Stm s) {print(s, 0); say("\n");}
+		// void print(Exp e) {print(e, 0); say("\n");}
+
+		// for compatibility
+		void prStm(Stm s, int d=0) { print(s, d); }
+		void prExp(Exp e, int d=0) { print(e, d); }
 	};
 }
